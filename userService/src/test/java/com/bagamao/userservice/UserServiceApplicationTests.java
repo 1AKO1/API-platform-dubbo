b2786a7dc3977e622b766c913a1adbd55522a61d
aira <1418407007@qq.com>
2023-08-16 19:07:33 +0800

    原本的代码有问题，orderService引入了seata后配置了，可能有什么问题导致orderService无法工作。现在修正到业务可以运行

--- userService/src/test/java/com/bagamao/userservice/UserServiceApplicationTests.java
+++ userService/src/test/java/com/bagamao/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,25 @@
 package com.bagamao.userservice;
 
+import com.alibaba.nacos.shaded.org.checkerframework.checker.units.qual.A;
+import com.alibaba.nacos.shaded.org.checkerframework.checker.units.qual.Acceleration;
+import com.bagamao.apicommon.service.AccountService;
+import com.bagamao.apicommon.service.OrderInfoService;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
+    @DubboReference
+    private OrderInfoService orderInfoService;
+    @DubboReference
+    private AccountService accountService;
     @Test
-    void contextLoads() {
+    void contextLoads() throws InterruptedException {
+        orderInfoService.generateOrder(2L, 2L);
+//        accountService.addAccount(1L, 5L);
     }
 
 }
